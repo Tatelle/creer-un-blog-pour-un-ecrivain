41f5bbb36b4d394bf1d2f79e7f3802cfcfd30474
Christelle Fau <fau.christelle.17@gmail.com>
2018-04-25 15:26:05 +0200

    Insertion des liens JS de Bootstrap à la fin de la page de template

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -26,6 +26,26 @@
     </head>
         
     <body>
+    	<nav class="navbar navbar-inverse navbar-fixed-top">
+	      	<div class="container">
+		        <div class="navbar-header">
+		          	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+		            	<span class="sr-only">Toggle navigation</span>
+		            	<span class="icon-bar"></span>
+		            	<span class="icon-bar"></span>
+		            	<span class="icon-bar"></span>
+		          	</button>
+		          	<a class="navbar-brand" href="#">Project name</a>
+		        </div>
+		        <div id="navbar" class="collapse navbar-collapse">
+		          	<ul class="nav navbar-nav">
+		            	<li class="active"><a href="#">Home</a></li>
+		            	<li><a href="#about">About</a></li>
+		            	<li><a href="#contact">Contact</a></li>
+		         	 </ul>
+		        </div><!--/.nav-collapse -->
+	      	</div>
+	    </nav>
     	<?php
 		if (!isset($_SESSION['login'])) //On est dans la page de formulaire
 		{ ?>
@@ -50,5 +70,13 @@
 		?>
 
         <?= $content ?>
+        
+        <!-- Bootstrap core JavaScript
+	    ================================================== -->
+	    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	    <script>window.jQuery || document.write('<script src="vendor/bootstrap/docs/assets/js/vendor/jquery.min.js"><\/script>')</script>
+	    <script src="vendor/bootstrap/docs/dist/js/bootstrap.min.js"></script>
+	    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	    <script src="vendor/bootstrap/docs/assets/js/ie10-viewport-bug-workaround.js"></script>
     </body>
 </html>
