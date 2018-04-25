b208720b7c0e8c5d6affa95d127feaf17c49d924
Christelle Fau <fau.christelle.17@gmail.com>
2018-04-25 15:04:24 +0200

    installation du dossier bootstrap et mise en place des liens sur le header

--- view/frontend/template.php
+++ view/frontend/template.php
@@ -2,8 +2,27 @@
 <html>
     <head>
         <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    	<meta name="viewport" content="width=device-width, initial-scale=1" />
+    	<meta name="description" content="Jean Forteroche, acteur et écrivain. Il travaille actuellement sur son prochain roman, 'Billet simple pour l'Alaska'. Il souhaite innover et le publier par épisode en ligne sur son propre site.">
+    	<meta name="author" content="Site web fait en PHP et MySQL. Avec une architecture MVC et programmé en parie en orienté objet. Le site a été développé pour le projet 4 : 'Créez le blog d'un écrivain', dans le cadre de la formation Openclassrooms 'Développeur Web Junior'">
+
         <title><?= $title ?></title>
+
+        <link href="vendor/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="vendor/bootstrap/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet" />
+        <link href="vendor/bootstrap/docs/examples/starter-template/starter-template.css" rel="stylesheet" />
         <link href="public/css/style.css" rel="stylesheet" /> 
+        <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
+	    <!--[if lt IE 9]><script src="vendor/bootstrap/docs/assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+	    <script src="vendor/bootstrap/docs/assets/js/ie-emulation-modes-warning.js"></script>
+
+	    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	    <!--[if lt IE 9]>
+	      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+	      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	    <![endif]-->
+
     </head>
         
     <body>
